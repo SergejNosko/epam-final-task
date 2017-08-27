16b4dd84a0c819dde1563e782210223dfd106b35
SergejNosko <tuzemec81@gmail.com>
2017-08-28 00:08:16 +0300

    add drop-down menu

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-let search = document.getElementById('search');
+let search = document.getElementById('search'),
+    popUp = document.getElementById('pop-up');
 
 search.addEventListener('click', (e) => {
     e.preventDefault();
@@ -6,4 +7,10 @@ search.addEventListener('click', (e) => {
     let searchField = document.getElementById('search-field');
     if(getComputedStyle(searchField).display == 'none') searchField.style.display = 'block';
     else searchField.style.display = 'none';
+});
+
+popUp.addEventListener('click', () => {
+   let headerBottom = document.getElementById('header-bottom');
+   if(getComputedStyle(headerBottom).display == 'none') headerBottom.style.display = 'flex';
+   else headerBottom.style.display = 'none';
 });
