54a26d7dea33c3cca7009663866ec06d5d9f261a
SergejNosko <tuzemec81@gmail.com>
2017-08-28 15:05:19 +0300

    add sliding images without timer

--- js/bundle.js
+++ js/bundle.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var search = document.getElementById('search'),
-    popUp = document.getElementById('pop-up');
+    popUp = document.getElementById('pop-up'),
+    prevButton = document.getElementById('prev-button'),
+    nextButton = document.getElementById('next-button');
 
 search.addEventListener('click', function (e) {
     e.preventDefault();
@@ -14,3 +16,17 @@ popUp.addEventListener('click', function () {
     var headerBottom = document.getElementById('header-bottom');
     if (getComputedStyle(headerBottom).display == 'none') headerBottom.style.display = 'flex';else headerBottom.style.display = 'none';
 });
+
+/*------------------------Slider--------------------------------------------------------*/
+
+function handleClickNext() {
+    var current = document.querySelector('.current-slide'),
+        next = current.nextElementSibling;
+    console.log(next);
+}
+function handleClickPrev() {
+    console.log('prev');
+}
+
+prevButton.addEventListener('click', handleClickPrev);
+nextButton.addEventListener('click', handleClickNext);
--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 let search = document.getElementById('search'),
-    popUp = document.getElementById('pop-up');
+    popUp = document.getElementById('pop-up'),
+    prevButton = document.getElementById('prev-button'),
+    nextButton = document.getElementById('next-button');
 
 search.addEventListener('click', (e) => {
     e.preventDefault();
@@ -14,3 +16,41 @@ popUp.addEventListener('click', () => {
    if(getComputedStyle(headerBottom).display == 'none') headerBottom.style.display = 'flex';
    else headerBottom.style.display = 'none';
 });
+
+/*------------------------Slider--------------------------------------------------------*/
+
+function handleClickNext() {
+    let current, next,
+        slides = document.getElementsByClassName('slider-item');
+    for(let i = 0; i < slides.length; i++){
+        if(slides[i].classList.contains('current-slide')){
+            current = i;
+            if(i !== slides.length - 1) next = i + 1;
+            else next = 0;
+        }
+    }
+
+    slides[current].style.animation = 'fade-out .5s ease forwards';
+    slides[current].classList.remove('current-slide');
+    slides[next].style.animation = 'fade-in .5s ease forwards';
+    slides[next].classList.add('current-slide');
+}
+function handleClickPrev() {
+    let current, prev,
+        slides = document.getElementsByClassName('slider-item');
+    for(let i = 0; i < slides.length; i++){
+        if(slides[i].classList.contains('current-slide')){
+            current = i;
+            if(i !== 0) prev = i - 1;
+            else prev = slides.length - 1;
+        }
+    }
+
+    slides[current].style.animation = 'fade-out .5s ease forwards';
+    slides[current].classList.remove('current-slide');
+    slides[prev].style.animation = 'fade-in .5s ease forwards';
+    slides[prev].classList.add('current-slide');
+}
+
+prevButton.addEventListener('click', handleClickPrev);
+nextButton.addEventListener('click', handleClickNext);
