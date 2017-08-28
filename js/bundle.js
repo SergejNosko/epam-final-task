641a7c476df99c7735a37b5403ed2b25528ea87b
SergejNosko <tuzemec81@gmail.com>
2017-08-28 11:54:58 +0300

    changed styles for supporting ie11

--- js/bundle.js
+++ js/bundle.js
@@ -1,1 +1,16 @@
-"use strict";
+'use strict';
+
+var search = document.getElementById('search'),
+    popUp = document.getElementById('pop-up');
+
+search.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    var searchField = document.getElementById('search-field');
+    if (getComputedStyle(searchField).display == 'none') searchField.style.display = 'block';else searchField.style.display = 'none';
+});
+
+popUp.addEventListener('click', function () {
+    var headerBottom = document.getElementById('header-bottom');
+    if (getComputedStyle(headerBottom).display == 'none') headerBottom.style.display = 'flex';else headerBottom.style.display = 'none';
+});
