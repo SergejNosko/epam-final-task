8ac234d800fd3db65ee80fd3d82b12372dbe6a14
SergejNosko <tuzemec81@gmail.com>
2017-08-30 12:43:44 +0300

    finished catalog page

--- js/bundle-filters.js
+++ js/bundle-filters.js
@@ -1,25 +1,26 @@
+'use strict';
+
 ;(function () {
-    let filter = document.getElementById('filter'),
+    var filter = document.getElementById('filter'),
         filterTablet = document.getElementById('filterTablet'),
         filterHeaders = document.getElementById('filter-headers').children;
 
     function handleFilter(e) {
-        let target = e.target;
+        var target = e.target;
 
-        if(target.tagName !== "LI") return;
+        if (target.tagName !== "LI") return;
 
-        let filterValue = target.parentElement.previousElementSibling,
+        var filterValue = target.parentElement.previousElementSibling,
             filterName = filterValue.previousElementSibling,
             parent = filterValue.parentElement;
-        if(target.textContent === 'Not selected'){
+        if (target.textContent === 'Not selected') {
             filterValue.style.display = 'none';
             parent.style.padding = '25px 20px';
             filterName.style.fontSize = '16px';
             filterName.style.marginBottom = '0px';
             target.parentElement.style.margin = '25px 0 0 0';
             //filterName.style.margin = '0 0 20px 0';
-        }
-        else{
+        } else {
             filterValue.textContent = target.textContent;
             filterValue.style.display = 'block';
             filterName.style.fontSize = '12px';
@@ -30,23 +31,21 @@
     }
 
     function handleFilterTablet(e) {
-        let target = e.target,
+        var target = e.target,
             itemsContainer = this.children[1];
-        console.log(itemsContainer);
 
-        if(getComputedStyle(itemsContainer).display === 'none') itemsContainer.style.display = 'flex';
-        else itemsContainer.style.display = 'none';
+        if (getComputedStyle(itemsContainer).display === 'none') itemsContainer.style.display = 'flex';else itemsContainer.style.display = 'none';
 
-        if(target.tagName !== "LI") return;
+        if (target.tagName !== "LI") return;
         itemsContainer.style.display = 'flex';
 
-        let targetId = target.parentElement.dataset.id,
+        var targetId = target.parentElement.dataset.id,
             parent = target.parentElement.parentElement;
 
-        if(target.textContent === 'Not selected'){
+        if (target.textContent === 'Not selected') {
             filterHeaders[targetId].textContent = target.parentElement.previousElementSibling.textContent + ' , ';
             filterHeaders[targetId].style.color = '#000';
-        }else{
+        } else {
             filterHeaders[targetId].textContent = target.textContent;
             filterHeaders[targetId].style.color = '#f14a58';
         }
@@ -54,4 +53,4 @@
 
     filter.addEventListener('click', handleFilter);
     filterTablet.addEventListener('click', handleFilterTablet);
-}());
+})();
